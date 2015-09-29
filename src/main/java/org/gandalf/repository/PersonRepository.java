35c400a3ebc83cf890fbfac4676b5dc115b6091d
subin <petersn@126.com>
2015-09-29 22:13:54 +0800

    update

--- src/main/java/org/gandalf/repository/PersonRepository.java
+++ src/main/java/org/gandalf/repository/PersonRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "person", path = "person")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 	
-	
 }
