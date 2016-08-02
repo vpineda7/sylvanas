8ca22e010071e7300634ad209a9c2b0e37f7e478
subin <petersn@126.com>
2016-08-02 22:10:21 +0800

    pycharm

--- Hello.py
+++
@@ -1,9 +0,0 @@
-import http.client
-
-conn = http.client.HTTPSConnection("www.python.org")
-
-conn.request("GET", "/")
-
-r1 = conn.getresponse()
-
-print(r1.status, r1.reason, r1.read())
--- Help.py
+++ Help.py
@@ -1,5 +1,17 @@
 # 模块说明
 
+# 操作系统接口
+# import os
+# import shutil
+
+# 数学
+# import math
+# import random
+
+# 日期和时间
+# from datetime import date
+# now = date.today()
+
 # 浏览器控制器
 # import webbrowser
 # webbrowser.open_new_tab(url)
@@ -9,10 +21,17 @@
 # file = urllib.request.urlopen('http://www.python.org/')
 # print(file.read().decode('utf-8'))
 
-# HTTP 客户端
-
+# HTTP客户端
 # import http.client
 # conn = http.client.HTTPSConnection("www.python.org")
 # conn.request("GET", "/")
 # r1 = conn.getresponse()
 # print(r1.status, r1.reason, r1.read())
+
+# 日志
+# import logging
+# logging.debug('Debugging information')
+# logging.info('Informational message')
+# logging.warning('Warning:config file %s not found', 'server.conf')
+# logging.error('Error occurred')
+# logging.critical('Critical error -- shutting down')
