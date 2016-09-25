bdbbfb9c6c69279f516f4e82f166c85b88bed0ec
subin <petersn@126.com>
2016-09-25 13:40:02 +0800

    chat

--- ChatServer.py
+++ ChatServer.py
@@ -1,20 +1,172 @@
-#!/bin/python
+#!/usr/bin/env python
+# coding=utf-8
 
 from asyncore import dispatcher
+from asynchat import async_chat
 import socket, asyncore
 
 PORT = 5005
+NAME = 'Aragorn Chat'
 
+class EndSession(Exception):
+    pass
+
+# 代理命令
+class CommandHandler:
+    def unknown(self, session, cmd):
+        session.push('unknown command: %s \r\n' % cmd)
+    def handle(self, session, line):
+        if not line.strip():
+            return
+        # 命令 参数
+        parts = line.split(' ', 1)
+        cmd = parts[0]
+
+        try:
+            line = parts[1].strip()
+        except IndexError:
+            line = ''
+
+        meth = getattr(self, 'do_'+cmd, None)
+
+        try:
+            meth(session, line)
+        except TypeError:
+            self.unknown(session, cmd)
+
+# 用户会话泛型环境
+class Room(CommandHandler):
+
+    def __init__(self,server):
+        self.server=server
+        self.sessions=[]
+
+    def add(self, session):
+        self.sessions.append(session)
+
+    def remove(self, session):
+        self.sessions.remove(session)
+
+    def broadcast(self, line):
+        for session in self.sessions:
+            session.push(line)
+
+    def do_logout(self, session, line):
+        raise EndSession
+
+# 登录用户房间
+class LoginRoom(Room):
+    def add(self, session):
+        Room.add(self, session)
+        self.broadcast('Welcome To %s \r\n' % self.server.name)
+
+    def unknown(self, session, cmd):
+        session.push('please login \r\n')
+
+    def do_login(self, session, line):
+
+        name = line.strip()
+        if not name:
+            session.push('please input user name \r\n')
+        elif name in self.server.users:
+            session.push('%s has exists. \r\n' % name)
+        else:
+            session.name=name
+            session.enter(self.server.main_room)
+            session.push('%s login success. \r\n' % name)
+
+# 多用户聊天房间
+class ChatRoom(Room):
+
+    def add(self, session):
+        self.broadcast(session.name+' in room. \r\n')
+        self.server.users[session.name]=session
+        Room.add(self, session)
+
+    def remove(self, session):
+        Room.remove(self, session)
+        self.broadcast(session.name +' out room. \r\n')
+
+    def do_say(self, session, line):
+        self.broadcast(session.name+':'+line+' \r\n')
+
+    def do_look(self, session, line):
+        session.push('all users of room: \r\n')
+        for other in self.sessions:
+            session.push(other.name+' \r\n')
+
+    def do_who(self, session, line):
+        session.push('all users of server: \r\n')
+        for name in self.server.users:
+            session.push(name+' \r\n')
+
+# 退出用户房间
+class LogoutRoom(Room):
+    def add(self, session):
+        try:
+            del self.server.users[session.name]
+        except KeyError:
+            pass
+
+# 用户通信
+class ChatSession(async_chat):
+
+    def __init__(self, server, sock):
+        async_chat.__init__(self, sock)
+        self.server = server
+        self.set_terminator("\r\n")
+        self.data=[]
+        self.name=None
+        self.enter(LoginRoom(server))
+
+    def enter(self, room):
+        try:
+            cur=self.room
+        except AttributeError:
+            pass
+        else:
+            cur.remove(self)
+        self.room = room
+        room.add(self)
+
+    def collect_incoming_data(self, data):
+        self.data.append(data)
+
+    def found_terminator(self):
+        line=''.join(self.data)
+        self.data=[]
+        try:
+            self.room.handle(self, line)
+        except EndSession:
+            self.handle_close()
+
+    def handle_close(self):
+        async_chat.handle_close(self)
+        self.enter(LogoutRoom(self.server))
+
+# 聊天服务器
 class ChatServer(dispatcher):
+
+    def __init__(self, port, name):
+        dispatcher.__init__(self)
+
+        self.create_socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind(('',port))
+        self.listen(5)
+
+        self.name=name
+        self.users={}
+        self.main_room=ChatRoom(self)
 
     def handle_accept(self):
         conn, addr = self.accept()
-        print 'Connection attempt from', addr[0]
+        ChatSession(self, conn)
 
-s = ChatServer()
-s.create_socket(socket.AF_INET,socket.SOCK_STREAM)
-
-s.bind(('',PORT))
-s.listen(5)
-
-asyncore.loop()
+####
+if __name__ == '__main__':
+    s = ChatServer(PORT, NAME)
+    try:
+        asyncore.loop()
+    except KeyboardInterrupt:
+        print
