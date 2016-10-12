d05e08326d8b6c0845712070b056035a1455b07f
subin <petersn@126.com>
2016-10-12 21:41:54 +0800

    re

--- crawling/company.py
+++ crawling/company.py
@@ -1,10 +1,40 @@
 #!/usr/bin/python
 #coding=utf-8
 
-# URL-拉钩企业信息-工商
+import urllib
+import re
+from pymongo import MongoClient
 
 # 获取文档
+def getDocument(url):
+    #post
+    #param = urllib.urlencode({"username":"1016903103@qq.com","password":"XXXX"})
+    #request = urllib.request(url, param)
+    #get
+    #values={}
+    #values['hello'] = 'world'
+    #param = urllib.urlencode(values)
+    #url = url +'?'+param
+    #headers = {'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+    response = urllib.urlopen(url)
+
+    return response.read()
 
 # 检索指定内容
+def getElement(document):
+    # 正则表达式
+    print document
 
 # 存储
+def save(collection):
+    client = MongoClient()
+
+# URL-拉钩企业信息-工商
+def main():
+    url="http://www.lagou.com/gongsi"
+    doc=getDocument(url)
+    ele=getElement(doc)
+    save(ele)
+
+if __name__ == "__main__":
+    main()
